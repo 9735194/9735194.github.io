cab257e94dd7a34071078628b37dcc2f13c52ad7
Serge Rivest <serge.rivest@gmail.com>
2019-10-27 22:48:54 +1100

    deleting junk

--- js/app.js
+++
@@ -1,7 +0,0 @@
-// Event listeners
-
-document.querySelector('#openCamera').addEventListener('click', startVideo);
-document.querySelector('#closeCamera').addEventListener('click', stopVideo);
-document.querySelector('#takeSnapshot').addEventListener('click', takeSnapshot);
-
-
